9afb606fea25c2ce30979581920c61bed80d9c0e
namrata.t <prachi.easternts@gmail.com>
2023-02-01 12:15:42 +0530

    crud operations in vue2 using axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+axios.defaults.headers.common = {
+  "X-Requested-With": "XMLHttpRequest",
+  "X-CSRFToken": "example-of-custom-header",
+  "Access-Control-Allow-Origin": '*'
+};
+
+axios.defaults.headers.post['xsrfCookieName'] = 'CSRFToken';
+axios.defaults.headers.post['xsrfHeaderName'] = 'X-CSRFToken';
+axios.defaults.headers.post['responseType'] = 'json';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
